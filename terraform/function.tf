bc5c23c6a13570bbbd49f85b406c7dca6d582692
Data Science Team <datascienceteam@Datas-MacBook-Pro.local>
2022-06-16 11:25:59 +0100

    aaa

--- terraform/function.tf
+++ terraform/function.tf
@@ -37,6 +37,19 @@ resource "google_cloudfunctions_function" "function" {
     entry_point           = "backup_connectivity_git"
 
     trigger_http          = true
+
+
+    # IAM entry for all users to invoke the function
+    resource "google_cloudfunctions_function_iam_member" "invoker" {
+      project        = google_cloudfunctions_function.function.project
+      region         = google_cloudfunctions_function.function.region
+      cloud_function = google_cloudfunctions_function.function.name
+
+      role   = "roles/cloudfunctions.invoker"
+      member = "allUsers"
+    }
+    
+
     #https_trigger_url    = "https://us-central1-dft-dst-prt-connectivitymetric.cloudfunctions.net/function-backup-connectivity-git"
     
 
